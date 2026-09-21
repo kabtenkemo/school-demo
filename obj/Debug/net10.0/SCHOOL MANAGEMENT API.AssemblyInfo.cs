aca535f01cbdde47030529915e4b9e514a5e0cfd
Ahmed Eed <Ahmed.Eed@ELSEWEDY-SCHOOL.LOCAL>
2026-09-21 14:37:58 +0300

    mapping2

--- obj/Debug/net10.0/SCHOOL MANAGEMENT API.AssemblyInfo.cs
+++ obj/Debug/net10.0/SCHOOL MANAGEMENT API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SCHOOL MANAGEMENT API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ceb378319172d515994801d67a032b34bf81e693")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7862a5bab35a26d125f03b09daea8308984a4109")]
 [assembly: System.Reflection.AssemblyProductAttribute("SCHOOL MANAGEMENT API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SCHOOL MANAGEMENT API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
